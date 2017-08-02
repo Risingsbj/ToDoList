eb163720d12093ef3321ab871dce529bc4e9a7ac
risingsbj <243281121@qq.com>
2017-08-02 17:29:55 +0800

    点击注册后使用 LeanCloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -4,9 +4,35 @@ var APP_ID = 'CjcJOdkC4oDGaRqLCl6fVW55-gzGzoHsz';
 var APP_KEY = 'hqk9AqzNmuwN4dlr8T3hCIFl';
 
 AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
+  	appId: APP_ID,
+  	appKey: APP_KEY
 });
 
 
 export default AV
+
+export function signUp(username, password, successFn, errorFn){
+   	// 新建 AVUser 对象实例
+  	var user = new AV.User()
+  	// 设置用户名
+  	user.setUsername(username)
+  	// 设置密码
+  	user.setPassword(password)
+  	// 设置邮箱
+  	user.signUp().then(function (loginedUser) {
+  	  	let user = getUserFromAVUser(loginedUser)
+  	  	successFn.call(null, user)
+ 	 }, function (error) {
+  	  	errorFn.call(null, error)
+  	})
+
+  	return undefined
+
+}
+
+function getUserFromAVUser(AVUser){
+  	return {
+   	 	id: AVUser.id,
+    	...AVUser.attributes
+    }
+}
