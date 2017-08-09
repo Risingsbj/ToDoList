6d4affe0cc2df0af1d8f82fec6366a613a03f8e9
risingsbj <243281121@qq.com>
2017-08-09 21:20:50 +0800

    将忘记密码表单也抽离成一个组件另外修改SignUpForm和SignInForm

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -3,20 +3,20 @@ import React, {Component} from 'react'
 export default class SignInForm extends Component {
 	render() {
 		return (
-			<form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
+			<form className="signIn" onSubmit={this.props.onSubmit}> {/* 登录*/}
           <div className="row">
             <label>用户名</label>
-              <input type="text" value={this.state.formData.username}
-                onChange={this.changeFormData.bind(this, 'username')}/>
+              <input type="text" value={this.props.formData.username}
+                onChange={this.props.onChange.bind(null, 'username')}/>
           </div>
           <div className="row">
             <label>密码</label>
-              <input type="password" value={this.state.formData.password}
-                onChange={this.changeFormData.bind(this, 'password')}/>
+              <input type="password" value={this.props.formData.password}
+                onChange={this.props.onChange.bind(null, 'password')}/>
           </div>
           <div className="row actions">
               <button type="submit">登录</button>
-              <a href="#" onClick={this.showForgotPassword.bind(this)}>忘记密码了？</a>
+              <a href="#" onClick={this.props.onForgotPassword}>忘记密码了？</a>
           </div>
       </form>	
 		)
--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -3,22 +3,22 @@ import React, {Component} from 'react'
 export default class SignUpForm extends Component {
 	render() {
 		return (
-			<form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
+			<form className="signUp" onSubmit={this.props.onSubmit.bind(this)}> {/* 注册*/}
             <div className="row">
                 <label>邮箱</label> 
-                <input type="text" value={this.state.formData.email}
-                 onChange={this.changeFormData.bind(this, 'email')}/>
+                <input type="text" value={this.props.formData.email}
+                 onChange={this.props.onChange.bind(this, 'email')}/>
             </div>
             <div className="row">
           			<label>用户名</label> 
-          			<input type="text" value={this.state.formData.username}
-          			 onChange={this.changeFormData.bind(this, 'username')}/>
+          			<input type="text" value={this.props.formData.username}
+          			 onChange={this.props.onChange.bind(null, 'username')}/>
           			{/*bind不仅可以绑定this,还可以绑定第一个参数*/}
         		</div>
         		<div className="row">
          			 <label>密码</label>
-          			<input type="password" value={this.state.formData.password}
-          			  onChange={this.changeFormData.bind(this, 'password')}/>
+          			<input type="password" value={this.props.formData.password}
+          			  onChange={this.props.onChange.bind(null, 'password')}/>
         		</div>
         		<div className="row actions">
           			<button type="submit">注册</button>
