fb7c33d9c64f91f391e3cecdc090992435dea96f
risingsbj <243281121@qq.com>
2017-08-10 22:08:05 +0800

    copy and run

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './TodoInput.css'
  
 function submit(props, e){
     if (e.key === 'Enter') {
-      	props.onSubmit(e)
+		props.onSubmit(e)
     }
 }
 function changeTitle(props, e){
     props.onChange(e)
 }
+// let temp = function(e){
+// changeTitle.call(null, props, e)
+// }
+
+// onChange={temp}
 // 搞清楚 bind 用法
 export default function(props) {
 	return <input type="text" value={props.content}
